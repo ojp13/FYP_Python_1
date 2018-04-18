8281207340c13f0a6433092a7beb904d877e98a4
ojp13 <peace.oscar@gmail.com>
2018-04-18 10:58:30 +0000

    SVM version 1 complete

--- FYP_Python_1/Dev_App_V1/svm_classifier.py
+++ FYP_Python_1/Dev_App_V1/svm_classifier.py
@@ -6,7 +6,7 @@ import numpy as np
 style.use('ggplot')
 
 class Support_Vector_Machine:
-    def __init__(self, visualization=True):
+    def __init__(self, visualization=True): #Visualisation is in case we want to plot our classifier
         self.visualization = visualization
         self.colors = {1:"r",-1:"b"}
         if self.visualization:
@@ -16,10 +16,16 @@ class Support_Vector_Machine:
     def fit(self, data):
         # Training the algorithm based on fed in feature data
         self.data = data
-
+        
+        #The aim of this bit of code is to choose the best values for the vectors w and b that will guarantee
+        #The best performance on future data. This is equivalent to choosing the decision boundary/separating
+        #Hyperplane that is the furthest distance from both support vectors
+        #We have asserted that the support vectors satisfy the equation yi(xi.w+b)=0, and that we want to min
+        #The magnitude of w
+        
         opt_dict = {} #{ ||w||: [w,b] } Dictionary, key is magnitude of w, values are w and b
 
-        #Transforms applied to the w/b vectors to compensate for feature signs
+        #Transforms applied to the w vectors, as the vector elements can have different signs but give same mag
         transforms = [[1,1],
                       [-1,1],
                       [-1,-1],
@@ -35,8 +41,8 @@ class Support_Vector_Machine:
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
 
-        all_data = None
-
+        all_data = None #So we don't have to carry around the data in memory
+        
         #For optimisation of SVM
         step_sizes = [self.max_feature_value * 0.1,
                       self.max_feature_value * 0.01,
@@ -44,7 +50,42 @@ class Support_Vector_Machine:
 
         #Optimising the bias value
         b_range_multiple = 5
-        
+        b_multiple = 5
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            optimized = False
+            while not optimized:
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
+                                   step.b_multiple):
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option = True
+
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                    found_option = False
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print("Optimised a step")
+                else:
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step*2
+            
+                    
         
 
     def predict(self, features):
